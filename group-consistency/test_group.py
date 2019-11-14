4001b6fedfb84719f56998cf8a452007f9b7da70
holger krekel <holger@merlinux.eu>
2019-11-14 15:42:21 +0100

    snap

--- group-consistency/test_group.py
+++ group-consistency/test_group.py
@@ -1,4 +1,5 @@
 
+import random
 import pytest
 from itertools import count
 
@@ -31,6 +32,7 @@ class Contact:
         if chat is None:
             chat = Chat(chat_id)
             self._chats[chat_id] = chat
+            chat.add_contact(self, self)
         chat.receive_log(log)
 
     def __eq__(self, other):
@@ -51,7 +53,7 @@ class Contact:
 
 class Chat:
     def __init__(self, chat_id):
-        self.id = id
+        self.id = chat_id
         self._known_message_ids = set()
         self.log =[]
 
@@ -78,6 +80,10 @@ class Chat:
         chat.add_contact(contact, contact)
         return chat
 
+    def add_contact_and_send(self, contact, other_contact):
+        self.add_contact(contact, other_contact)
+        self.send_out_last_log()
+
     def add_contact(self, contact, other_contact):
         if other_contact in self.members:
             return
@@ -91,11 +97,13 @@ class Chat:
         self.log.append(Entry(message_id(), contact, "del", other_contact))
 
     def send_out_last_log(self):
-        entry = self.log[-1]
         for member in self.members[1:]:
-            member.receive_log(chat_id=self.id, log=self.log[-1:])
+            member.receive_log(chat_id=self.id, log=self.log[-10:])
 
     def receive_log(self, log):
+        if random.random() > 1.8:
+            print("{}: failed randomly to receive log len={}".format(self, len(log)))
+            return
         for entry in log:
             if entry.message_id not in self._known_message_ids:
                 if entry.op == "add":
@@ -105,21 +113,22 @@ class Chat:
                 self._known_message_ids.add(entry.message_id)
 
 def test_membership_basic():
-
     contact0 = Contact("zero")
     chat = Chat.create_new(10, contact0)
-    chat.add_contact(contact0, Contact("alice"))
-    chat.send_out_last_log()
-    chat.add_contact(contact0, Contact("bob"))
-    chat.send_out_last_log()
-    chat.add_contact(contact0, Contact("carol"))
-    chat.send_out_last_log()
-
+    chat.add_contact_and_send(contact0, Contact("alice"))
+    chat.add_contact_and_send(contact0, Contact("bob"))
+    chat.add_contact_and_send(contact0, Contact("carol"))
     assert len(chat.members) == 4
 
     assert chat.members[0] == contact0
     assert chat.members[-1].addr == "carol"
     chat.remove_contact(contact0, chat.members[-1])
+    chat.send_out_last_log()
+
     assert len(chat.members) == 3
     with pytest.raises(ValueError):
         chat.remove_contact(contact0, Contact("unknown"))
+
+    # ensure consistent member list
+    for member in chat.members[1:]:
+        assert len(member._chats[chat.id].members) == len(chat.members)
